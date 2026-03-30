6039ad41604f74122e2076348ffbc0d79579f0ba
alejandro <henry.giraldo@est.iudigital.edu.co>
2026-03-29 22:09:25 -0500

    servicios con api en la nube

--- src/core/axiosClient.js
+++ src/core/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: "http://localhost:4000/api",
+    baseURL: "https://peliculas-api-3.onrender.com/api",
 });
 
 export default axiosClient;
